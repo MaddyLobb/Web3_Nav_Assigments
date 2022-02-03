33646b0d2f2e38849624cb2b4e587ea78d7a5a35
MaddyLobb <81719007+MaddyLobb@users.noreply.github.com>
2022-02-03 12:15:23 -0500

    menu finally started!!!

--- dev/js/menu.js
+++
@@ -1,9 +0,0 @@
-import { gsap } from "gsap";
-
-const growTL = new gsap.timeline();
-growTL.to("#rectangle1",{duration:0.5, scaleX: 2});
-
-export const navTL = new gsap.timeline({paused:true});
-
-
-navTL.add(growTL,"burger")
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,9 +1,35 @@
-import { navTL } from "./menu";
+var li_items = document.querySelectorAll(".accordion_wrap ul li");
+var ul = document.querySelector(".accordion_wrap ul");
 
-var rectangleButton = document.querySelector("#burger");
-
-
-rectangleButton.addEventListener("click", () =>{
-
-    navTL.play();
+li_items.forEach(function(item){
+	item.addEventListener("click", function(){
+		li_items.forEach(function(item){
+			item.classList.remove("active");
+		})
+		item.classList.add("active");
+	});
 });
+
+ul.addEventListener("mouseleave", function(){
+	li_items.forEach(function(item){
+		item.classList.remove("active");
+	})
+});
+
+
+// code to follow w3 school 
+// var accordion = document.getElementsByClassName("accordion")
+
+// var i;
+
+// for (i = 0; i < accordion.length; i++){
+//     accordion[i].addEventListener("click", function(){ 
+//         this.classList.toggle ("active");
+//         var panel = this.nextElementSibling;
+//         if (panel.style.maxWidth) {
+//             panel.style.maxWidth = "null";
+//         } else {
+//         panel.style.maxWidth = panel.scrollWidth + "px";
+//         }  
+//     });
+// }
